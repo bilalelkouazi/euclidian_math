90c4528441bb43d9f87845177667a89854d5a718
bilal <b.kouazi@gmail.com>
2019-05-17 15:38:19 +0000

    clean up

--- pythogorean_theorem.c
+++ pythogorean_theorem.c
@@ -14,11 +14,4 @@ double c=a*a+b*b;
 printf("Squared A: %lf\nSquared B: %lf\nEquals %lf which is C squared\nTherefore C equals %lf\n", a*a, b*b, a*a+b*b, sqrt(c));
 
 return 0;
-/*
-Ask a
-Ask b
-a²+b²=c²
-square c
-exit
-*/
 }
